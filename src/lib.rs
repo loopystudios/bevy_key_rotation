0da92deb7ff3ef64fda9f662f736356803fdbf33
Spencer C. Imbleau <spencer@imbleau.com>
2023-10-20 10:39:52 -0400

    build: publicized types

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,9 @@ mod error;
 mod plugin;
 mod systems;
 
+// Re-export instant
+pub use instant::{Duration, Instant};
+
+pub use data_types::{AuthProvider, KeyRotationSettings, Keystore, KeystoreState};
+pub use error::TokenRotationError;
 pub use plugin::KeyRotationPlugin;
