a6e828ad7e07a271c1842c42366e4aab424e1460
Spencer C. Imbleau <spencer@imbleau.com>
2023-10-20 22:11:30 -0400

    build: TokenRotationError::new

--- src/error.rs
+++ src/error.rs
@@ -3,3 +3,9 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 #[error(transparent)]
 pub struct TokenRotationError(#[from] Box<dyn std::error::Error + Send>);
+
+impl TokenRotationError {
+    pub fn new(source: impl std::error::Error + Send + 'static) -> Self {
+        Self(Box::new(source))
+    }
+}
